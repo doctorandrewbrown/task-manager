c74ebe551335f3ed39190e4a2f81df2c3fb7499c
andrew brown <dr.andrew.david.brown@gmail.com>
2024-06-10 17:54:57 +0000

    adds Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # string representation for tasks in admin
+    def __str__(self):
+        return self.name
+
